329ea381d89c5e5a50318ab068bc140f4f9d03b8
AliceA248 <98560120+AliceA248@users.noreply.github.com>
2024-01-16 10:13:07 -0300

    melhorias do layout da page dos projetos

--- src/app/pageProject/ProjectDetails.js
+++ src/app/pageProject/ProjectDetails.js
@@ -2,17 +2,23 @@
 import React from 'react';
 import { useParams, useLocation } from 'react-router-dom';
 import "./style.css";
+import linkr from "../../assets/images/linkr.png"
 
 const projectDetailsMap = {
-  '"Rede social de compartilhamento de links': {
-    descricao: 'Compartilhamento de links, interação social, cadastro e login seguro.',
+  'Rede social de compartilhamento de links': {
+    descricao: 'A Rede Social de Compartilhamento é uma plataforma dinâmica que visa unir usuários por meio de interesses comuns, oferecendo uma experiência atraente e interativa.A interface do usuário foi projetada com foco na experiência do usuário, garantindo que a plataforma seja acessível e funcional em uma variedade de dispositivos e tamanhos de tela',
+    funcionalidades: "Design Responsivo, Pesquisa Avançada, Interação Social, Compartilhamento de links",
     tecnologias: 'React, Node.js, Express, PostgreSQL',
+    image: linkr,
   },
-  'outro-projeto': {
-    descricao: 'Descrição personalizada para outro projeto.',
-    tecnologias: 'Tecnologias específicas desse projeto',
+  'Sistema para gerenciamento de eventos': {
+    descricao: 'Sistema White Label para gerenciamento de eventos. Inscrições simples, opções de hospedagem, formas de pagamento flexíveis, seleção de atividades personalizadas e emissão de certificados de participação.',
+    tecnologias: 'Node.js, Express, PostgreSQL, React, Styled components',
   },
-  // Adicione mais projetos conforme necessário
+  'Aplicação completa de acompanhamento de hábitos': {
+    descricao: 'Com uma interface intuitiva, você pode facilmente adicionar novos hábitos e fazer um acompanhamento, fornecendo descrições detalhadas e estabelecendo datas para começar',
+    tecnologias: '',
+  },
 };
 
 const ProjectDetails = () => {
@@ -20,7 +26,6 @@ const ProjectDetails = () => {
   const location = useLocation();
   const { videoUrl } = location.state || {};
 
-  // Obtenha os detalhes do projeto com base no nome do projeto
   const details = projectDetailsMap[projectName] || {};
 
   return (
@@ -34,7 +39,16 @@ const ProjectDetails = () => {
       <div className="project-content">
         <h2>Informações do Projeto</h2>
         <p className='descricao'>{details.descricao}</p>
+        <p className='descricao'>{details.funcionalidades}</p>
         <p className='tecnologias'>{details.tecnologias}</p>
+
+      </div>
+      <div className="project-image">
+        {[1, 2, 3, 4, 5].map((_, index) => (
+          <div key={index} className="image-container">
+            <img className="linkr" src={details.image} alt={`Linkr ${index + 1}`} />
+          </div>
+        ))}
       </div>
     </div>
   );
