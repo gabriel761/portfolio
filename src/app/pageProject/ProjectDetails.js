5cb75223af9ff90d9209fbfb4b106168a558616c
AliceA248 <98560120+AliceA248@users.noreply.github.com>
2024-01-16 10:35:44 -0300

    map em todas as fotos da const projectDetails-projectDetails

--- src/app/pageProject/ProjectDetails.js
+++ src/app/pageProject/ProjectDetails.js
@@ -3,21 +3,30 @@ import React from 'react';
 import { useParams, useLocation } from 'react-router-dom';
 import "./style.css";
 import linkr from "../../assets/images/linkr.png"
+import linkrCadastro from "../../assets/images/linkr - cadastro.jpg"
+import linkrPesquisaUsuario from "../../assets/images/linkr - pesquisa usuário.png"
+import linkTimeline from "../../assets/images/linkr - timeline.png"
+import linkUserPosts from "../../assets/images/linkr - userposts.png"
+
+
+
 
 const projectDetailsMap = {
   'Rede social de compartilhamento de links': {
-    descricao: 'A Rede Social de Compartilhamento é uma plataforma dinâmica que visa unir usuários por meio de interesses comuns, oferecendo uma experiência atraente e interativa.A interface do usuário foi projetada com foco na experiência do usuário, garantindo que a plataforma seja acessível e funcional em uma variedade de dispositivos e tamanhos de tela',
+    descricao: 'A Rede Social de Compartilhamento é uma plataforma dinâmica que visa unir usuários por meio de interesses comuns, oferecendo uma experiência atraente e interativa. A interface do usuário foi projetada com foco na experiência do usuário, garantindo que a plataforma seja acessível e funcional em uma variedade de dispositivos e tamanhos de tela.',
     funcionalidades: "Design Responsivo, Pesquisa Avançada, Interação Social, Compartilhamento de links",
     tecnologias: 'React, Node.js, Express, PostgreSQL',
-    image: linkr,
+    images: [linkrCadastro, linkr, linkrPesquisaUsuario, linkTimeline, linkUserPosts],
   },
   'Sistema para gerenciamento de eventos': {
     descricao: 'Sistema White Label para gerenciamento de eventos. Inscrições simples, opções de hospedagem, formas de pagamento flexíveis, seleção de atividades personalizadas e emissão de certificados de participação.',
     tecnologias: 'Node.js, Express, PostgreSQL, React, Styled components',
+    images: [/* Adicione as imagens correspondentes a este projeto */],
   },
   'Aplicação completa de acompanhamento de hábitos': {
     descricao: 'Com uma interface intuitiva, você pode facilmente adicionar novos hábitos e fazer um acompanhamento, fornecendo descrições detalhadas e estabelecendo datas para começar',
     tecnologias: '',
+    images: [/* Adicione as imagens correspondentes a este projeto */],
   },
 };
 
@@ -41,12 +50,11 @@ const ProjectDetails = () => {
         <p className='descricao'>{details.descricao}</p>
         <p className='descricao'>{details.funcionalidades}</p>
         <p className='tecnologias'>{details.tecnologias}</p>
-
       </div>
       <div className="project-image">
-        {[1, 2, 3, 4, 5].map((_, index) => (
+        {details.images.map((image, index) => (
           <div key={index} className="image-container">
-            <img className="linkr" src={details.image} alt={`Linkr ${index + 1}`} />
+            <img className="linkr" src={image} alt={`${projectName} ${index + 1}`} />
           </div>
         ))}
       </div>
